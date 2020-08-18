5fc6a7ead2d316548094a0295def0e885e15a435
samyleedev <68584701+samyleedev@users.noreply.github.com>
2020-08-18 11:52:06 +0200

    UPDATE
    - ConnexionJSP
    - ConnexionServlet

---
+++ WebContent/WEB-INF/pages/pageConnexion.jsp
@@ -0,0 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Connexion</title>
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+	integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+	crossorigin="anonymous">
+</head>
+<body>
+
+	<h1>ENI-Enchères</h1>
+
+	<form class="col-3" method="POST" action="Connexion">
+	
+		<div class="form-group ">
+			<label for="identifiant">Identifiant :</label> 
+			<input type="text" class="form-control" name="identifiantInput" id="utilisateur" aria-describedby="emailHelp">
+		</div>
+		
+		<div class="form-group">
+			<label for="motDePasse">Mot de passe :</label> 
+		    <input type="password" class="form-control" name="motDePasseInput" id="motDePasse">
+		</div>
+		
+		<a href="#">Mot de passe oublié</a>
+		
+		<div class="form-group form-check">
+			<input type="checkbox" class="form-check-input" id="exampleCheck1">
+			<label class="form-check-label" for="exampleCheck1">Se souvenir de moi</label>
+		</div>
+		
+		<button type="submit" class="btn btn-primary">Connexion</button>
+		
+	</form>
+
+	<button type="button" class="btn btn-primary">Créer un compte</button>
+	
+	<c:out value="${identifiant }"></c:out>
+	<c:out value="${motDePasse }"></c:out>
+
+</body>
+</html>
---
+++ src/fr/eni/encheres/ihm/PageConnexionServlet.java
@@ -0,0 +1,61 @@
+package fr.eni.encheres.ihm;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class PageConnexionServlet
+ */
+@WebServlet("/Connexion")
+public class PageConnexionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PageConnexionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/pages/pageConnexion.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+//		String identifiant = request.getParameter("identifiantInput");
+//		String motDePasse = request.getParameter("motDePasseInput");
+//		
+//		request.setAttribute("identifiant", identifiant);
+//		request.setAttribute("motDePasse", motDePasse);
+
+		
+		HttpSession session = request.getSession();
+		
+		String identifiant = request.getParameter("identifiantInput");
+		String motDePasse = request.getParameter("motDePasseInput");
+		
+		session.setAttribute("identifiant", identifiant);
+		session.setAttribute("motDePasse", motDePasse);
+
+		
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/pages/pageConnexion.jsp").forward(request, response);
+
+	}
+
+}
