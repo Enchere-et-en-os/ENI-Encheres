2a0710028cfdfb356202fad26774703af49cb155
samyleedev <68584701+samyleedev@users.noreply.github.com>
2020-08-19 17:09:22 +0200

    Update UtilisateurDAOImpl
    - Finition parfaite du SelectById

--- src/fr/eni/encheres/dal/UtilisateurDAOImpl.java
+++ src/fr/eni/encheres/dal/UtilisateurDAOImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 import fr.eni.encheres.bo.Utilisateur;
 
 public class UtilisateurDAOImpl implements UtilisateurDAO {
-	
+
 	/**
 	 * Attributs de classe des requêtes sql
 	 */
@@ -25,20 +25,18 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 			+ " telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) values(?,?,?,?,?,?,?,?,?,?,?)";
 
 	private static final String SELECT_ID_BY_PSEUDO = "SELECT no_utilisateur FROM UTILISATEURS WHERE pseudo = ?";
-	
 
 	/**
 	 * Attributs de classe des requêtes sql
 	 */
 	private static final String SQL_SELECT_ALL_USER = "SELECT * from UTILISATEURS";
-	
+
 	/**
 	 * méthode pour récupérer tous les utilisateurs en base de donnée
 	 */
 	public List<Utilisateur> findAllUtilisateur() throws DALException {
 		List<Utilisateur> listeUtilisateur = new ArrayList<Utilisateur>();
 
-		
 		try (Connection conn = ConnectionProvider.getConnection()) {
 			Statement stmt = conn.createStatement();
 
@@ -60,32 +58,31 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 
 	}
 
-	
 	/*
 	 * @auhtor : Valentin
 	 * 
 	 * @param : String pseudo
 	 * 
 	 * Selection l'id d'un utilisateur si son param existe
-	 * */
-	public int findPseudo(String pseudo) throws DALException{
+	 */
+	public int findPseudo(String pseudo) throws DALException {
 		int id = 0;
-		
+
 		try (Connection conn = ConnectionProvider.getConnection()) {
 			Statement stmt = conn.createStatement();
-			
+
 			ResultSet rs = stmt.executeQuery(SELECT_ID_BY_PSEUDO);
-			if(rs.next()) {
+			if (rs.next()) {
 				id = rs.getInt("no_utilisateur");
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new DALException("Echec de findPseudo",  e);
+			throw new DALException("Echec de findPseudo", e);
 		}
 		return id;
 	}
-	
+
 	/*
 	 * @auhtor : Valentin
 	 * 
@@ -93,12 +90,12 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 	 * 
 	 * Insertion en bdd de l'utilisateur via inscription
 	 */
-	
-	public void insertUtilisateur(Utilisateur user) throws DALException{
-		try (Connection conn = ConnectionProvider.getConnection()){
+
+	public void insertUtilisateur(Utilisateur user) throws DALException {
+		try (Connection conn = ConnectionProvider.getConnection()) {
 			System.out.println("dal");
 			PreparedStatement pstmt = conn.prepareStatement(SQL_INSERT_USER, PreparedStatement.RETURN_GENERATED_KEYS);
-			
+
 			pstmt.setString(1, user.getPseudo());
 			pstmt.setString(2, user.getNom());
 			pstmt.setString(3, user.getPrenom());
@@ -108,13 +105,13 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 			pstmt.setString(7, user.getCodePostal());
 			pstmt.setString(8, user.getVille());
 			pstmt.setString(9, user.getMotDePasse());
-			pstmt.setInt(10,100);
+			pstmt.setInt(10, 100);
 			pstmt.setInt(11, 0);
 			pstmt.executeUpdate();
-			
+
 			ResultSet rs = pstmt.getGeneratedKeys();
-			if(rs.next()) {
-				user.setId(rs.getInt(1));	
+			if (rs.next()) {
+				user.setId(rs.getInt(1));
 			}
 
 		} catch (SQLException e) {
@@ -124,8 +121,6 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 		}
 
 	}
-
-		
 
 	/*
 	 * @auhtor : Samy-Lee
@@ -137,31 +132,26 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 	public Utilisateur selectById(int id) throws DALException {
 		ResultSet rs = null;
 		Utilisateur util = null;
-		System.out.println("avant try");
-		
+
 		try (Connection cnx = ConnectionProvider.getConnection();) {
-			
+
 			PreparedStatement pstmt = cnx.prepareStatement(SQL_SELECT_BY_ID);
 			pstmt.setInt(1, id);
-			
-
 			rs = pstmt.executeQuery();
-			
 
-			
 			if (rs.next()) {
-				
+
 				util = new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo"), rs.getString("nom"),
 						rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"), rs.getString("rue"),
 						rs.getString("code_postal"), rs.getString("ville"), rs.getString("mot_de_passe"),
 						rs.getInt("credit"), rs.getBoolean("administrateur"));
+			} else {
+				throw new DALException("L'utilisateur vaut null");
 			}
 		} catch (SQLException e) {
 			throw new DALException(
 					"Erreur lors de l'éxécution de la méthode SelectById de la classe UtilisateurDAOImpl", e);
 		}
-
-		
 		return util;
 
 	}
